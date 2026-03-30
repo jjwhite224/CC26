f8c33a0f51a64b5d07dafe292a2fce07dd2c6038
jjwhite224 <jw3513@nyu.edu>
2026-03-30 15:29:28 -0400

    Hypertext Haiku

--- Hypertext Narrative/style.js
+++ Hypertext Narrative/style.js
@@ -1,16 +1,25 @@
 const page = document.querySelector(".page");
 const hoverWords = document.querySelectorAll(".hover-word");
+const resetBtn = document.querySelector(".reset-bg");
 
 const defaultGif = page.dataset.defaultGif;
+let locked = false;
 page.style.backgroundImage = `url("${defaultGif}")`;
 
 hoverWords.forEach(word => {
+  const gif = word.dataset.gif;
+
   word.addEventListener("mouseenter", () => {
-    const gif = word.dataset.gif;
-    page.style.backgroundImage = `url("${gif}")`;
+    if (!locked) page.style.backgroundImage = `url("${gif}")`;
   });
 
   word.addEventListener("mouseleave", () => {
-    page.style.backgroundImage = `url("${defaultGif}")`;
+    if (!locked) page.style.backgroundImage = `url("${defaultGif}")`;
+  });
+
+  word.addEventListener("click", () => {
+    locked = true;
+    page.style.backgroundImage = `url("${gif}")`;
   });
 });
+
